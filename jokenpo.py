b0183d59f9f26ee04d589a9d432aeea8ec5845b8
Yuri Souza <yurisoouza123@gmail.com>
2026-05-03 18:22:19 -0400

    Update: save user's nickname in the game

--- jokenpo.py
+++ jokenpo.py
@@ -5,6 +5,11 @@ itens = ('Pedra', 'Papel', 'Tesoura')
 while True:
     computador = randint(0, 2)
 
+    nome = input("Digite seu nome: ")
+    sleep(3)
+    print('Olá, {}! Vamos jogar Jokenpô?'.format(nome))
+    sleep(3)
+    
     print('''Suas opções:
 [ 0 ] PEDRA
 [ 1 ] PAPEL
@@ -19,15 +24,15 @@ while True:
     print('PO!!!')
     print('-=' * 11)
 
-    print('O computador jogou {}'.format(itens[computador]))
-    print('O jogador jogou {}'.format(itens[jogador]))
+    print('Computador jogou {}'.format(itens[computador]))
+    print('{} jogou {}'.format(nome, itens[jogador]))
     print('-=' * 11)
 
     if computador == 0:  # PEDRA
         if jogador == 0: #jogador jogou pedra
             print('EMPATE!')
         elif jogador == 1: #jogador jogou papel
-            print('JOGADOR VENCE!')
+            print('{} VENCE!'.format(nome))
         elif jogador == 2: #jogador jogou tesoura
             print('COMPUTADOR VENCE!')
 
@@ -37,11 +42,11 @@ while True:
         elif jogador == 1: #jogador jogou papel
             print('EMPATE!')
         elif jogador == 2: #jogador jogou tesoura
-            print('JOGADOR VENCE!')
+            print('{} VENCE!'.format(nome))
 
     elif computador == 2:  # TESOURA
         if jogador == 0: #jogador jogou pedra
-            print('JOGADOR VENCE!')
+            print('{} VENCE!'.format(nome))
         elif jogador == 1: #jogador jogou papel
             print('COMPUTADOR VENCE!')
         elif jogador == 2: #jogador jogou tesoura
